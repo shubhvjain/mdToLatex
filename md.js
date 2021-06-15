f9bd293e323401fa751641d85fe1b47815f4f034
Shubh V Jain <shubhvjain@gmail.com>
2021-06-15 14:46:31 +0530

    added recursive parser for list processing

--- md.js
+++ md.js
@@ -1,272 +1,5 @@
 const marked = require("marked")
 
-let a = [
-    {
-     "type": "list",
-     "raw": "- list 1\n    - sub list 1\n        - item 1\n            1. sample 1\n            2. sample 2\n- list 2\n\n",
-     "ordered": false,
-     "start": "",
-     "loose": false,
-     "items": [
-      {
-       "type": "list_item",
-       "raw": "- list 1\n    - sub list 1\n        - item 1\n            1. sample 1\n            2. sample 2\n",
-       "task": false,
-       "loose": false,
-       "text": "list 1\n  - sub list 1\n      - item 1\n          1. sample 1\n          2. sample 2",
-       "tokens": [
-        {
-         "type": "text",
-         "raw": "list 1",
-         "text": "list 1",
-         "tokens": [
-          {
-           "type": "text",
-           "raw": "list 1",
-           "text": "list 1"
-          }
-         ]
-        },
-        {
-         "type": "list",
-         "raw": "  - sub list 1\n      - item 1\n          1. sample 1\n          2. sample 2",
-         "ordered": false,
-         "start": "",
-         "loose": false,
-         "items": [
-          {
-           "type": "list_item",
-           "raw": "  - sub list 1\n      - item 1\n          1. sample 1\n          2. sample 2",
-           "task": false,
-           "loose": false,
-           "text": "sub list 1\n  - item 1\n      1. sample 1\n      2. sample 2",
-           "tokens": [
-            {
-             "type": "text",
-             "raw": "sub list 1",
-             "text": "sub list 1",
-             "tokens": [
-              {
-               "type": "text",
-               "raw": "sub list 1",
-               "text": "sub list 1"
-              }
-             ]
-            },
-            {
-             "type": "list",
-             "raw": "  - item 1\n      1. sample 1\n      2. sample 2",
-             "ordered": false,
-             "start": "",
-             "loose": false,
-             "items": [
-              {
-               "type": "list_item",
-               "raw": "  - item 1\n      1. sample 1\n      2. sample 2",
-               "task": false,
-               "loose": false,
-               "text": "item 1\n  1. sample 1\n  2. sample 2",
-               "tokens": [
-                {
-                 "type": "text",
-                 "raw": "item 1",
-                 "text": "item 1",
-                 "tokens": [
-                  {
-                   "type": "text",
-                   "raw": "item 1",
-                   "text": "item 1"
-                  }
-                 ]
-                },
-                {
-                 "type": "list",
-                 "raw": "  1. sample 1\n  2. sample 2",
-                 "ordered": true,
-                 "start": 1,
-                 "loose": false,
-                 "items": [
-                  {
-                   "type": "list_item",
-                   "raw": "  1. sample 1\n",
-                   "task": false,
-                   "loose": false,
-                   "text": "sample 1",
-                   "tokens": [
-                    {
-                     "type": "text",
-                     "raw": "sample 1",
-                     "text": "sample 1",
-                     "tokens": [
-                      {
-                       "type": "text",
-                       "raw": "sample 1",
-                       "text": "sample 1"
-                      }
-                     ]
-                    }
-                   ]
-                  },
-                  {
-                   "type": "list_item",
-                   "raw": "  2. sample 2",
-                   "task": false,
-                   "loose": false,
-                   "text": "sample 2",
-                   "tokens": [
-                    {
-                     "type": "text",
-                     "raw": "sample 2",
-                     "text": "sample 2",
-                     "tokens": [
-                      {
-                       "type": "text",
-                       "raw": "sample 2",
-                       "text": "sample 2"
-                      }
-                     ]
-                    }
-                   ]
-                  }
-                 ]
-                }
-               ]
-              }
-             ]
-            }
-           ]
-          }
-         ]
-        }
-       ]
-      },
-      {
-       "type": "list_item",
-       "raw": "- list 2\n",
-       "task": false,
-       "loose": false,
-       "text": "list 2",
-       "tokens": [
-        {
-         "type": "text",
-         "raw": "list 2",
-         "text": "list 2",
-         "tokens": [
-          {
-           "type": "text",
-           "raw": "list 2",
-           "text": "list 2"
-          }
-         ]
-        }
-       ]
-      }
-     ]
-    },
-    {
-     "type": "list",
-     "raw": "1. item list 1\n    - sample 1\n    - sample 2\n2. item list 2\n\n",
-     "ordered": true,
-     "start": 1,
-     "loose": false,
-     "items": [
-      {
-       "type": "list_item",
-       "raw": "1. item list 1\n    - sample 1\n    - sample 2\n",
-       "task": false,
-       "loose": false,
-       "text": "item list 1\n - sample 1\n - sample 2",
-       "tokens": [
-        {
-         "type": "text",
-         "raw": "item list 1",
-         "text": "item list 1",
-         "tokens": [
-          {
-           "type": "text",
-           "raw": "item list 1",
-           "text": "item list 1"
-          }
-         ]
-        },
-        {
-         "type": "list",
-         "raw": " - sample 1\n - sample 2",
-         "ordered": false,
-         "start": "",
-         "loose": false,
-         "items": [
-          {
-           "type": "list_item",
-           "raw": " - sample 1\n",
-           "task": false,
-           "loose": false,
-           "text": "sample 1",
-           "tokens": [
-            {
-             "type": "text",
-             "raw": "sample 1",
-             "text": "sample 1",
-             "tokens": [
-              {
-               "type": "text",
-               "raw": "sample 1",
-               "text": "sample 1"
-              }
-             ]
-            }
-           ]
-          },
-          {
-           "type": "list_item",
-           "raw": " - sample 2",
-           "task": false,
-           "loose": false,
-           "text": "sample 2",
-           "tokens": [
-            {
-             "type": "text",
-             "raw": "sample 2",
-             "text": "sample 2",
-             "tokens": [
-              {
-               "type": "text",
-               "raw": "sample 2",
-               "text": "sample 2"
-              }
-             ]
-            }
-           ]
-          }
-         ]
-        }
-       ]
-      },
-      {
-       "type": "list_item",
-       "raw": "2. item list 2\n\n",
-       "task": false,
-       "loose": false,
-       "text": "item list 2",
-       "tokens": [
-        {
-         "type": "text",
-         "raw": "item list 2",
-         "text": "item list 2",
-         "tokens": [
-          {
-           "type": "text",
-           "raw": "item list 2",
-           "text": "item list 2"
-          }
-         ]
-        }
-       ]
-      }
-     ]
-    }
-   ]
-
 let textParser = {
     "text":(itm,options={})=>{
         // todo look for math here 
@@ -306,7 +39,29 @@ let parsers = {
 
      },
     list: (itm,options={}) => {
-
+        let processList =  (obj)=>{
+            let command = "itemize"
+            if(obj.ordered){command = "enumerate"}
+            let steps = ``
+            obj.items.map(item=>{
+                let currItem = `\\item `
+                item.tokens.map(tkn=>{
+                    if(tkn.type=="text"){
+                        // normal text , include it after the item keyword
+                        currItem += tkn.text
+                    }
+                    if(tkn.type=="list"){
+                        // list needs to be processsed recuursively 
+                        let subList = processList(tkn)
+                        currItem += "\n"+subList
+                    }
+                })
+                steps += `${currItem} \n`
+            })
+            let str = "\\begin{"+command+"}\n"+steps+"\n"+"\\end{"+command+"}"
+            return str
+        }
+        return processList(itm)
      },
     blockquote: (itm,options={}) => {
 
@@ -334,11 +89,14 @@ let toLatex = (mdString, options = {}) => {
                 parts.push(parsers[item.type](item))
             }
         })
-        console.log(parts)
-        let doc = `
-\\documentclass[a4paper,12pt]{article} 
+       // console.log(parts)
+      //  console.log()
+        let doc = `\\documentclass[a4paper,12pt]{article} 
+
+${packagesRequired.map(pkg=>{return `\\usepackage{${pkg.name}} \n`}).join("")}
 \\begin{document}
 
+${parts.join("\n")}
 
 \\end{document}`
 
@@ -349,12 +107,9 @@ let toLatex = (mdString, options = {}) => {
     }
 }
 
-let sample = `
-# Title of the article 
+let sample = `# Title of the article 
 
 ## Subtitle of the article 
-
-some text will come here 
 
 this is another paragraph 
 
@@ -363,10 +118,36 @@ this is another paragraph with a list
 - sammple list 2
 - sample list 3
 
+> sample
+
 numbered list
 1. sample
 2. sample 3
 3. sample4
+
+**this** para [contains](link) inline ~~things~~. this ^will^ be used here  \`code here\` ![img](somethinghere)
+
+![sample](/sample)
+
+\`\`\`
+some code here
+then more here
+more ;
+\`\`\`
+
+- list 1
+	- sub list 1
+		- item 1
+			1. sample 1
+            2. sample 2
+- list 2
+
+1. item list 1
+	- sample 1
+	- sample 2
+2. item list 2
+
+
 `
 
 console.log(toLatex(sample))
