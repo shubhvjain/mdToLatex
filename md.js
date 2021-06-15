df8ed5183a624b693ef0d1c46a4b13656f7e4807
Shubh V Jain <shubhvjain@gmail.com>
2021-06-15 16:05:56 +0530

    format for parsers changed to object

--- md.js
+++ md.js
@@ -3,28 +3,29 @@ const marked = require("marked")
 let textParser = {
     "text": (itm, options = {}) => {
         // todo look for math here 
-        return itm.text
+        return { text: itm.text }
     },
     "strong": (itm, options = {}) => {
-        return ` \\textbf{${itm.text}} `
+        return { text: ` \\textbf{${itm.text}} ` }
     },
     "link": (itm, options = {}) => {
         let url = itm.href
         if (options.baseUrl) {
             // if url does not have a base url, then add one 
         }
-        return `\\href{${url}}{${itm.text}}`
+        return { text: `\\href{${url}}{${itm.text}}` }
     },
     "del": (itm, options = {}) => {
         // not supported
-        return itm.text
+        return { text: itm.text }
     },
     "codespan": (itm, options = {}) => {
         // inline code
-        return `\\verb|${itm.text}|`
+        return { text: `\\verb|${itm.text}|` }
     },
-    "image": (itm, options = {}) => { },
-    "strong": (itm, options = {}) => { }
+    "image": (itm, options = {}) => {
+        return {text:""}
+    }
 }
 
 let parsers = {
@@ -32,10 +33,12 @@ let parsers = {
         let title = `section`
         if (itm.depth == 2) { title = 'sub' + title }
         else if (itm.depth >= 3) { title = 'subsub' + title }
-        return `\\${title}{${itm.text}} `
+        return { text: `\\${title}{${itm.text}} ` }
     },
     paragraph: (itm, options = {}) => {
-
+        let para = itm.tokens.map(it=>{return textParser[it.type](it)})
+        // console.log(para)
+        return { text: itm.text }
     },
     list: (itm, options = {}) => {
         let processList = (obj) => {
@@ -57,16 +60,16 @@ let parsers = {
                 })
                 steps += `${currItem} \n`
             })
-            let str = ["\\begin{"+command+"}",steps,"\\end{" + command + "}" ].join("\n") 
+            let str = ["\\begin{" + command + "}", steps, "\\end{" + command + "}"].join("\n")
             return str
         }
-        return processList(itm)
+        return { text: processList(itm) }
     },
     blockquote: (itm, options = {}) => {
-
+        return { text: ["\\begin{displayquote}", '``' + itm.text + '``', "\\end{displayquote}"].join("\n") }
     },
     code: (itm, options = {}) => {
-        return  ["\\begin{lstlisting}",itm.text,"\\end{lstlisting}"].join("\n") 
+        return { text: ["\\begin{lstlisting}", itm.text, "\\end{lstlisting}"].join("\n") }
     }
 }
 
@@ -75,38 +78,35 @@ let toLatex = (mdString, options = {}) => {
         let packagesRequired = [
             { name: "hyperref" },
             { name: "listings" },
+            { name: "csquotes" }
         ]
         let tokens = marked.lexer(mdString)
         let parts = []
         tokens.map(item => {
             if (parsers[item.type]) {
+                //console.log(parsers[item.type](item))
                 parts.push(parsers[item.type](item))
             }
         })
-        // console.log(parts)
+        //console.log(parts)
         //  console.log()
         let doc = `\\documentclass[a4paper,12pt]{article} 
 
 ${packagesRequired.map(pkg => { return `\\usepackage{${pkg.name}} \n` }).join("")}
 \\begin{document}
 
-${parts.join("\n")}
+${parts.map(pt => { return pt.text }).join("\n")}
 
 \\end{document}`
-
-
         return doc
     } catch (error) {
-
+        console.log(error)
     }
 }
 
 let sample = `# Title of the article 
-
 ## Subtitle of the article 
-
 this is another paragraph 
-
 this is another paragraph with a list
 - sample list 1
 - sammple list 2
@@ -119,7 +119,7 @@ numbered list
 2. sample 3
 3. sample4
 
-**this** para [contains](link) inline ~~things~~. this ^will^ be used here  \`code here\` ![img](somethinghere)
+**this** para [contains](link) inline ~~things~~. this will be used here  \`code here\` ![img](somethinghere)
 
 ![sample](/sample)
 
