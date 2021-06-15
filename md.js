32dda3412412ba91e16eaca73240582f002a8b0a
Shubh V Jain <shubhvjain@gmail.com>
2021-06-15 15:05:34 +0530

    code formatting

--- md.js
+++ md.js
@@ -1,99 +1,93 @@
 const marked = require("marked")
 
 let textParser = {
-    "text":(itm,options={})=>{
+    "text": (itm, options = {}) => {
         // todo look for math here 
         return itm.text
     },
-    "strong":(itm,options={})=>{
+    "strong": (itm, options = {}) => {
         return ` \\textbf{${itm.text}} `
     },
-    "link":(itm,options={})=>{
+    "link": (itm, options = {}) => {
         let url = itm.href
-        if(options.baseImgUrl){
-            // if image does not have a base url, then add one 
+        if (options.baseUrl) {
+            // if url does not have a base url, then add one 
         }
-        // \href{http://www.overleaf.com}{Something Linky}
         return `\\href{${url}}{${itm.text}}`
     },
-    "del":(itm,options={})=>{
+    "del": (itm, options = {}) => {
         // not supported
         return itm.text
     },
-    "codespan":(itm,options={})=>{
+    "codespan": (itm, options = {}) => {
         // inline code
         return `\\verb|${itm.text}|`
     },
-    "image":(itm,options={})=>{},
-    "strong":(itm,options={})=>{}
+    "image": (itm, options = {}) => { },
+    "strong": (itm, options = {}) => { }
 }
 
 let parsers = {
-    heading: (itm,options={}) => {
+    heading: (itm, options = {}) => {
         let title = `section`
-        if(itm.depth==2){ title = 'sub'+title }
-        else if (itm.depth >= 3){ title = 'subsub'+title  }
+        if (itm.depth == 2) { title = 'sub' + title }
+        else if (itm.depth >= 3) { title = 'subsub' + title }
         return `\\${title}{${itm.text}} `
     },
-    paragraph: (itm,options={}) => {
+    paragraph: (itm, options = {}) => {
 
-     },
-    list: (itm,options={}) => {
-        let processList =  (obj)=>{
+    },
+    list: (itm, options = {}) => {
+        let processList = (obj) => {
             let command = "itemize"
-            if(obj.ordered){command = "enumerate"}
+            if (obj.ordered) { command = "enumerate" }
             let steps = ``
-            obj.items.map(item=>{
+            obj.items.map(item => {
                 let currItem = `\\item `
-                item.tokens.map(tkn=>{
-                    if(tkn.type=="text"){
+                item.tokens.map(tkn => {
+                    if (tkn.type == "text") {
                         // normal text , include it after the item keyword
                         currItem += tkn.text
                     }
-                    if(tkn.type=="list"){
+                    if (tkn.type == "list") {
                         // list needs to be processsed recuursively 
                         let subList = processList(tkn)
-                        currItem += "\n"+subList
+                        currItem += "\n" + subList
                     }
                 })
                 steps += `${currItem} \n`
             })
-            let str = "\\begin{"+command+"}\n"+steps+"\n"+"\\end{"+command+"}"
+            let str = ["\\begin{"+command+"}",steps,"\\end{" + command + "}" ].join("\n") 
             return str
         }
         return processList(itm)
-     },
-    blockquote: (itm,options={}) => {
+    },
+    blockquote: (itm, options = {}) => {
 
     },
-    code:(itm,options={})=>{
-        return `
-\\begin{lstlisting}
-${itm.text}
-\\end{lstlisting}
-`
+    code: (itm, options = {}) => {
+        return  ["\\begin{lstlisting}",itm.text,"\\end{lstlisting}"].join("\n") 
     }
 }
 
 let toLatex = (mdString, options = {}) => {
     try {
-        let packagesRequired= [
-            {name:"hyperref"},
-            {name:"listings"},
-
+        let packagesRequired = [
+            { name: "hyperref" },
+            { name: "listings" },
         ]
         let tokens = marked.lexer(mdString)
         let parts = []
-        tokens.map(item=>{
-            if(parsers[item.type]){
+        tokens.map(item => {
+            if (parsers[item.type]) {
                 parts.push(parsers[item.type](item))
             }
         })
-       // console.log(parts)
-      //  console.log()
+        // console.log(parts)
+        //  console.log()
         let doc = `\\documentclass[a4paper,12pt]{article} 
 
-${packagesRequired.map(pkg=>{return `\\usepackage{${pkg.name}} \n`}).join("")}
+${packagesRequired.map(pkg => { return `\\usepackage{${pkg.name}} \n` }).join("")}
 \\begin{document}
 
 ${parts.join("\n")}
