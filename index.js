967a798156f7ca3899fdc2b0148e4877438b1ba5
Shubh V Jain <shubhvjain@gmail.com>
2021-06-15 16:48:58 +0530

    added http server with post request

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const md = require("./md")
+const app = express();
+app.use(express.json())
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === 'OPTIONS') {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET")
+    return res.status(200).json({})
+  }
+  next();
+})
+app.get("/", (req, res) => {res.json({ "hello": "Hello world" })})
+
+app.post('/', async (req, res)=> {
+  try {
+    if(!req.body.options){throw new Error("Options not provided")}
+    if(!req.body.data){throw new Error("Data not provided")}
+    let options = req.body.options
+    let data =  req.body.data
+    if(typeof data != "string"){throw new Error("Invalid data")}
+    let tex =   md.toLatex(data,options)
+    res.json(tex)
+
+  } catch (error) {
+    console.log(error)
+    console.log("Error: "+error.message)
+    res.json({ "error": error.message })
+  }
+});
+
+const port = process.env.PORT || 3220
+app.set('port', port)
+app.listen(port, () => console.log(`App started on port ${port}.`))
