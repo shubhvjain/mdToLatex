1fa52451c93d77615619fc849b543f2585ec72f2
Shubh V Jain <shubhvjain@gmail.com>
2021-06-17 18:11:24 +0530

    chnages in  image parser added br text parser included option with all parser calls added image array in list tag added new method for use package

--- md.js
+++ md.js
@@ -1,33 +1,33 @@
 const marked = require("marked")
 
-let genImageName = (text)=>{
-    return `image_${ Math.floor((Math.random() * 10000) + 1)}`
+let genImageName = (text) => {
+    return `image_${Math.floor((Math.random() * 10000) + 1)}`
 }
 
 let textParser = {
     "text": (itm, options = {}) => {
         //console.log(itm.text)
         let txt = itm.text
-        .replace("\\","\\textbackslash}")
-        .replace(/&amp;/g,"\\&")
-        .replace(/&lt;/gm,`\\textless`)
-        .replace(/&gt;/gm,`\\textgreater`)
-        .replace(/&quot;/g,'"')
-        .replace(/&apos;/g,"'")
-        .replace(/&#39;/g,"'")
-        .replace("%","\\%")
-        .replace("{","\\{")
-        .replace("}","\\}")
-        .replace("^","\\textasciicircum")
-        .replace("~","\\textasciitilde")
-        .replace("#","\\#")
-        .replace("_","\\_")
+            .replace("\\", "\\textbackslash}")
+            .replace(/&amp;/g, "\\&")
+            .replace(/&lt;/gm, `\\textless`)
+            .replace(/&gt;/gm, `\\textgreater`)
+            .replace(/&quot;/g, '"')
+            .replace(/&apos;/g, "'")
+            .replace(/&#39;/g, "'")
+            .replace("%", "\\%")
+            .replace("{", "\\{")
+            .replace("}", "\\}")
+            .replace("^", "\\textasciicircum")
+            .replace("~", "\\textasciitilde")
+            .replace("#", "\\#")
+            .replace("_", "\\_")
         //   $ 
         // console.log(txt)
         return { text: txt }
     },
     "strong": (itm, options = {}) => {
-        return { text:  `\\textbf{${itm.text}} ` }
+        return { text: `\\textbf{${itm.text}} ` }
     },
     "link": (itm, options = {}) => {
         let url = itm.href
@@ -46,28 +46,28 @@ let textParser = {
     },
     "image": (itm, options = {}) => {
         let imgName = genImageName(itm.text)
-        let fig= `
-\\begin{figure}
-    \\includegraphics{${imgName}}
-    \\caption{${itm.text}}
-\\end{figure}`
+        let fig = `\\includegraphics[max size={\\textwidth}{\\textheight}]{${imgName}}`
+        let imgLink  = itm.href
+        if(imgLink.charAt(0)=="/" && options.baseImgUrl){
+            imgLink = options.baseImgUrl+imgLink
+        }
         return {
-            text:fig,
-            image:{
-                link:itm.href,
-                name:imgName
-            }
+            text: fig,
+            image: {link: imgLink,name: imgName}
         }
     },
-    "escape":(itm,options={}) =>{
-        return {text:``}
+    "escape": (itm, options = {}) => {
+        return { text: `` }
     },
-    "em":(itm,options={}) =>{
+    "em": (itm, options = {}) => {
         // todo modify this
-        return {text:`${itm.text}`}
+        return { text: `${itm.text}` }
     },
-    "displayMath":(itm,options={})=>{
+    "displayMath": (itm, options = {}) => {
         return itm.text
+    },
+    "br": (itm, options = {}) => {
+        return ``
     }
 }
 
@@ -79,20 +79,22 @@ let parsers = {
         return { text: `\\${title}{${itm.text}} ` }
     },
     paragraph: (itm, options = {}) => {
-        let para = itm.tokens.map(it=>{return textParser[it.type](it)})
+        let para = itm.tokens.map(it => { return textParser[it.type](it,options) })
         // console.log(para)
         let pText = []
         let imgArr = []
-        para.map(p=>{
+        para.map(p => {
             pText.push(p.text)
-            if(p.image){imgArr.push(p.image)}
+            if (p.image) { imgArr.push(p.image) }
         })
-        return { text:pText.join(""),images:imgArr }
+        return { text: pText.join(""), images: imgArr }
     },
     list: (itm, options = {}) => {
-        let processTextTokens = (tkns)=>{
-            let pa = tkns.map(tk=>{return textParser[tk.type](tk)})
-            return pa.map(p=>{return p.text}).join("")
+        let images = []
+        let processTextTokens = (tkns) => {
+            let pa = tkns.map(tk => { return textParser[tk.type](tk,options) })
+            pa.map(p => { if (p.image) { images = images.concat(p.image) } })
+            return pa.map(p => { return p.text }).join("")
         }
         let processList = (obj) => {
             let command = "itemize"
@@ -116,16 +118,17 @@ let parsers = {
             let str = ["\\begin{" + command + "}", steps, "\\end{" + command + "}"].join("\n")
             return str
         }
-        return { text: processList(itm) }
+        let list = processList(itm)
+        return { text: list, images: images }
     },
     blockquote: (itm, options = {}) => {
         return { text: ["\\begin{displayquote}", '``' + itm.text + '``', "\\end{displayquote}"].join("\n") }
     },
     code: (itm, options = {}) => {
-        return { text: ["\\begin{lstlisting}",itm.text, "\\end{lstlisting}"].join("\n") }
+        return { text: ["\\begin{lstlisting}", itm.text, "\\end{lstlisting}"].join("\n") }
     },
-    space:(itm,options={}) =>{
-        return {text:`\\newline`}
+    space: (itm, options = {}) => {
+        return { text: `\\newline` }
     }
 }
 
@@ -143,44 +146,61 @@ let parsers = {
 //       return false;
 //     }
 //   };
-  
+
 // marked.use({ tokenizer });
+
+let getRequiredPackages = () => {
+    let packagesRequired = [
+        {
+            name: "{hyperref}",
+            "config": `\\hypersetup{
+    colorlinks=true,
+    urlcolor=blue,
+    linkcolor=blue
+    }` },
+        { name: "{listings}" },
+        { name: "{csquotes}" },
+        { name: "{graphicx}" , config:`\\graphicspath{ {./images/} }`},
+        {name:"[export]{adjustbox}"}
+    ]
+    let packages = packagesRequired.map(pkg => {
+        let pk = `\\usepackage${pkg.name} \n`
+        if (pkg.config) {
+            pk += "\n" + pkg.config + "\n"
+        }
+        return pk
+    }).join("")
+    return packages
+}
+
+let getMDToken = (mdString)=>{
+    return marked.lexer(mdString)
+} 
 
 let toLatex = (mdString, options = {}) => {
     try {
-        let packagesRequired = [
-            { name: "hyperref",
-        "config":`\\hypersetup{
-colorlinks=true,
-urlcolor=blue,
-linkcolor=blue}` },
-            { name: "listings" },
-            { name: "csquotes" },
-            {name:"graphicx"}
-        ]
-        let tokens = marked.lexer(mdString)
-        console.log(JSON.stringify(tokens,null,2))
+        let tokens = getMDToken(mdString)
+        // console.log(JSON.stringify(tokens,null,2))
         let parts = []
         tokens.map(item => {
             // console.log(item.type)
             if (parsers[item.type]) {
-                parts.push(parsers[item.type](item))
+                parts.push(parsers[item.type](item,options))
             }
         })
-        let packages = packagesRequired.map(pkg => { 
-            let pk =  `\\usepackage{${pkg.name}} \n`
-            if(pkg.config){
-                pk += "\n"+pkg.config+"\n"
+
+        let imgArr = []
+        //  console.log(parts)
+        parts.map(pt => {
+            if (pt.images) {
+                imgArr = imgArr.concat(pt.images)
             }
-            return pk
-         }).join("")
-         let imgArr = []
-         parts.map(pt => { if(pt.image){imgArr.push(pt.image)} })
-         let data = {
-             content:parts.map(pt => { return pt.text }).join("\n"),
-             packages:packages,
-             images:imgArr
-         }
+        })
+        let data = {
+            content: parts.map(pt => { return pt.text }).join("\n"),
+            packages: getRequiredPackages(),
+            images: imgArr
+        }
         return data
     } catch (error) {
         console.log(error)
