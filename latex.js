e6e5619c044c9108c75962f7aceacfa483b534b4
Shubh V Jain <shubhvjain@gmail.com>
2021-06-17 18:51:12 +0530

    error handling

--- latex.js
+++ latex.js
@@ -3,25 +3,29 @@ const axios = require('axios').default;
 
 var md = require('./md')
 let downloadDependencies = async (mdString, options) => {
-    // given a md string (in string), get add image dependencies and download it as a zip folder
-    let tex = md.toLatex(mdString, options)
-    let imgBuffers = await downloadImage(tex.images)
-    let finalTexFile = ` 
+    try {
+        // given a md string (in string), get add image dependencies and download it as a zip folder
+        let tex = md.toLatex(mdString, options)
+        let imgBuffers = await downloadImage(tex.images)
+        let finalTexFile = ` 
 \\documentclass[a4paper,11pt]{article} 
 ${tex.packages}
 \\begin{document}
 ${tex.content}
 \\end{document}
     `
-    var zip = new AdmZip();
-    zip.addFile("main.tex",finalTexFile)
-    imgBuffers.map(img=>{
-        zip.addFile("images/"+img.fileName,img.image)
-    })
-    
-    var zipFileContents = zip.toBuffer();
-    return zipFileContents
-    // zip.writeZip("sample.zip");
+        var zip = new AdmZip();
+        zip.addFile("main.tex", finalTexFile)
+        imgBuffers.map(img => {
+            zip.addFile("images/" + img.fileName, img.image)
+        })
+
+        var zipFileContents = zip.toBuffer();
+        return zipFileContents
+        // zip.writeZip("sample.zip");    
+    } catch (error) {
+        throw error
+    }
 }
 
 downloadImage = async (images) => {
@@ -35,12 +39,16 @@ downloadImage = async (images) => {
 }
 
 getBase64Image = async (image) => {
-    // image {url,fileName}
-    let imgData = await axios.get(image.link, {responseType: 'arraybuffer'})
-    let fileType = imgData["headers"]["content-type"]
-    let parts = fileType.split("/")
-    let fileName = image.name+"."+parts[1]
-    return{image:imgData.data,fileType:fileType,fileName:fileName,name:image.name}
+    try {
+        // image {url,fileName}
+        let imgData = await axios.get(image.link, { responseType: 'arraybuffer' })
+        let fileType = imgData["headers"]["content-type"]
+        let parts = fileType.split("/")
+        let fileName = image.name + "." + parts[1]
+        return { image: imgData.data, fileType: fileType, fileName: fileName, name: image.name }
+    } catch (error) {
+        throw error
+    }
 }
 
 module.exports = {
